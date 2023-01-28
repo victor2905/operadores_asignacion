48d17f9fb9a5a0dba175f7115fc6d519305426b8
victor2905 <98538867+victor2905@users.noreply.github.com>
2023-01-27 22:47:10 -0500

    operadores_asignacion
    Creación de proyecto

---
+++ operadores_asignacion.py
@@ -0,0 +1,37 @@
+print("************************")
+print("Operadores de asignacion")
+print("************************\n")
+nombre = input("Ingrese su nombre: ") 
+X = 1;
+print("--- Ingremento o aumento ---")
+print("El valor inicial de X es: ",X)
+if X == 1:
+    X +=1
+
+if X == 2:
+    X +=1
+
+if X == 3:
+    X +=1
+
+if X == 4:
+    X +=1
+
+print("El valor final de X es: ",X)
+
+print("--- Decremento o disminucion ---")
+print("El valor inicial de X es: ",X)
+if X == 5:
+    X -=1
+
+if X == 4:
+    X -=1
+
+if X == 3:
+    X -=1
+
+if X == 2:
+    X -=1
+
+print("El valor final de X es: ",X)
+
